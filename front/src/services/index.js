e0a0dda78403f3b075fc1b3984cf4190ffafccde
Guibson Moura <mouraguibson12@gmail.com>
2025-12-22 17:32:53 -0400

    update auth front

---
+++ front/src/services/index.js
@@ -0,0 +1,6 @@
+export * from "./certificadosService";
+export * from "./empresasService";
+export * from "./gruposService";
+export * from "./usuariosService";
+export * from "./planosTrabalhoService";
+export * from "./timezoneService";
